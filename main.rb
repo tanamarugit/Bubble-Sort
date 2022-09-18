589524b1665db3c4255fe8eae32b800546d056f0
tanamaru <yt.0408.mmm@gmail.com>
2022-09-18 10:16:33 +0900

    debugを入れて実装

---
+++ main.rb
@@ -0,0 +1,20 @@
+require 'debug'
+number = [10, 8, 3, 5, 2, 4, 11, 18, 20, 33]
+
+number_count = number.size - 1
+loop do
+  s = 0
+  number_count.times do
+    l, r = number[-2-s], number[-1-s]
+    if l > r
+      number[-2-s] = r
+      number[-1-s] = l
+      binding.break 
+    end
+    s += 1
+  end
+  number_count -= 1
+  break if number_count.zero?
+end
+
+puts number
